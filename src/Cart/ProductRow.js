778a587a9f07cdc3e9cf60b18b1d09634502d0df
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2023-12-06 01:24:45 +1100

    fixed cart responsiveness

--- src/Cart/ProductRow.js
+++ src/Cart/ProductRow.js
@@ -1,35 +1,38 @@
-import '../styles/productrow.css';
-import DecrementBtn from '../DecrementBtn';
-import IncrementBtn from '../IncrementBtn';
+import "../styles/productrow.css";
+import DecrementBtn from "../DecrementBtn";
+import IncrementBtn from "../IncrementBtn";
 
 const ProductRow = (props) => {
-  return (
-    <div className="product-row">
-      <p>Product: {props.product}</p>
-      <p>Price: ${props.price}</p>
-      <div className="container">
-        <label htmlFor="quantity">
-          Quantity:
-          <input
-            id="quantity"
-            type="number"
-            value={props.quantity}
-            onChange={(e) => {
-              props.handleInputChange(props.product, e.target.value);
-            }}
-          />
-        </label>
-        <DecrementBtn
-          handleDecrement={props.handleDecrement}
-          product={props.product}
-        />
-        <IncrementBtn
-          handleIncrement={props.handleIncrement}
-          product={props.product}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="product-row">
+			<p className="product-item">Product: {props.product}</p>
+			<p className="product-item">Price: ${props.price}</p>
+			<div className="container product-quantity">
+				<label htmlFor="quantity">
+					Quantity:
+					<input
+						id="quantity"
+						type="number"
+						value={props.quantity}
+						onChange={(e) => {
+							props.handleInputChange(
+								props.product,
+								e.target.value
+							);
+						}}
+					/>
+				</label>
+				<DecrementBtn
+					handleDecrement={props.handleDecrement}
+					product={props.product}
+				/>
+				<IncrementBtn
+					handleIncrement={props.handleIncrement}
+					product={props.product}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default ProductRow;
