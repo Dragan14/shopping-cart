c6a70db2e26f8e3d7cbe16c86ff365e74611162d
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2023-12-06 00:55:12 +1100

    fixed navbar responsiveness

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,58 +1,58 @@
-import './styles/navbar.css';
-import { Link } from 'react-router-dom';
+import "./styles/navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ cartItems, handleCartClick }) => {
-  const totalItems = cartItems.reduce((a, b) => {
-    return a + b.quantity;
-  }, 0);
-  return (
-    <header className="navbar">
-      <img
-        className="navbar-icon"
-        src={require('./img/navbar-icon.png')}
-        alt="penguin"
-      />{' '}
-      <h1 className="title">
-        <Link to="/">Animal shop</Link>
-      </h1>
-      <ul className="nav-links-list">
-        <li className="nav-link">
-          <Link to="/">Home</Link>{' '}
-        </li>
-        <li className="nav-link">
-          {' '}
-          <Link to="/shop">Products</Link>{' '}
-        </li>
-        {cartItems.length > 0 && (
-          <div className="cart-icon-container" onClick={handleCartClick}>
-            <li>
-              <img
-                className="cart-icon"
-                src={require('./img/shopping-cart.png')}
-                alt=""
-              />
-            </li>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                position: 'absolute',
-                height: '25px',
-                width: '25px',
-                top: '-10px',
-                right: '-10px',
-                borderRadius: '50%',
-                border: '1px solid #000',
-              }}
-            >
-              {totalItems}
-            </div>
-          </div>
-        )}
-      </ul>
-    </header>
-  );
+	const totalItems = cartItems.reduce((a, b) => {
+		return a + b.quantity;
+	}, 0);
+	return (
+		<header className="navbar">
+			<Link to="/" className="logo-name">
+				<img
+					className="navbar-icon"
+					src={require("./img/navbar-icon.png")}
+					alt="penguin"
+				/>
+				<h1 className="title">Animal Store</h1>
+			</Link>
+			<ul className="nav-links-list navbar-links">
+				<li className="nav-link">
+					{" "}
+					<Link to="/shop">Shop</Link>{" "}
+				</li>
+				{cartItems.length >= 0 && (
+					<div
+						className="cart-icon-container"
+						onClick={handleCartClick}
+					>
+						<li>
+							<img
+								className="cart-icon"
+								src={require("./img/shopping-cart.png")}
+								alt=""
+							/>
+						</li>
+						<div
+							style={{
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								position: "absolute",
+								height: "25px",
+								width: "25px",
+								top: "-10px",
+								right: "-10px",
+								borderRadius: "50%",
+								border: "1px solid #000",
+							}}
+						>
+							{totalItems}
+						</div>
+					</div>
+				)}
+			</ul>
+		</header>
+	);
 };
 
 export default Navbar;
--- src/Shop/Product.js
+++ src/Shop/Product.js
@@ -1,48 +1,58 @@
-import IncrementBtn from '../IncrementBtn';
-import DecrementBtn from '../DecrementBtn';
-import '../styles/product.css';
+import IncrementBtn from "../IncrementBtn";
+import DecrementBtn from "../DecrementBtn";
+import "../styles/product.css";
 
 const Product = ({
-  product,
-  imgUrl,
-  price,
-  quantity,
-  handleAddToCart,
-  handleIncrement,
-  handleDecrement,
+	product,
+	imgUrl,
+	price,
+	quantity,
+	handleAddToCart,
+	handleIncrement,
+	handleDecrement,
 }) => {
-  let content;
+	let content;
 
-  if (quantity > 0) {
-    content = (
-      <p>
-        Quantity: {quantity}
-        <DecrementBtn handleDecrement={handleDecrement} product={product} />
-        <IncrementBtn handleIncrement={handleIncrement} product={product} />
-      </p>
-    );
-  } else {
-    content = (
-      <button
-        value={product}
-        className="cart-btn"
-        onClick={(e) => handleAddToCart(e.target.value, price)}
-      >
-        Add to cart
-      </button>
-    );
-  }
+	if (quantity > 0) {
+		content = (
+			<p>
+				Quantity: {quantity}
+				<DecrementBtn
+					handleDecrement={handleDecrement}
+					product={product}
+				/>
+				<IncrementBtn
+					handleIncrement={handleIncrement}
+					product={product}
+				/>
+			</p>
+		);
+	} else {
+		content = (
+			<button
+				value={product}
+				className="cart-btn"
+				onClick={(e) => handleAddToCart(e.target.value, price)}
+			>
+				Add to cart
+			</button>
+		);
+	}
 
-  return (
-    <div className="product">
-      <div className="img-container">
-        <img src={require(`../img/${imgUrl}`)} alt={product} />
-      </div>
-      <p>{product}</p>
-      <p>Price: ${price}</p>
-      {content}
-    </div>
-  );
+	return (
+		<div className="product">
+			<div className="img-container">
+				<img
+					className="product-image"
+					src={require(`../img/${imgUrl}`)}
+					alt={product}
+				/>
+			</div>
+			<p>{product}</p>
+			<p>Price: ${price}</p>
+			{content}
+		</div>
+	);
 };
 
 export default Product;
